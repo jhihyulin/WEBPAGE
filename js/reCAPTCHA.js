b23997aa124c3431aec59267b65c9d8e506392b5
jhih_yu <thomas.JY.Lin@gmail.com>
2021-07-16 16:13:45 +0800

    r

---
+++ js/reCAPTCHA.js
@@ -0,0 +1,4 @@
+const appCheck = firebase.appCheck();
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+appCheck.activate("6LfXK5wbAAAAAGMIB8-8zSBuvXjYmj-FCITlYa9x");
