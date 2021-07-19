a370f08d6daee7cd93b8cb4819f1d4c8ad244cfd
jhih_yu <thomas.JY.Lin@gmail.com>
2021-07-19 21:54:07 +0800

    darkmode

---
+++ js/darkmode.js
@@ -0,0 +1,20 @@
+function addDarkmodeWidget() {
+    new Darkmode().showWidget();
+}
+window.addEventListener('load', addDarkmodeWidget);
+const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.3s', // default: '0.3s'
+    mixColor: '#ffffff', // default: '#fff'
+    backgroundColor: '#ffffff', // default: '#fff'
+    buttonColorDark: '#000000', // default: '#100f2c'
+    buttonColorLight: '#ffffff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
