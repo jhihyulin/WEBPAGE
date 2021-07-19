9ea75386fee48ba706f315638b99561db2f11ed0
jhih_yu <thomas.JY.Lin@gmail.com>
2021-07-19 11:17:36 +0800

    login statue

--- js/login_check.js
+++ js/login_check.js
@@ -5,8 +5,10 @@ firebase.auth().onAuthStateChanged(function (user) {
         var email = user.email;
         var uid = user.uid;
         console.log(email, uid);
+        document.getElementById("login_statue").innerHTML="已登入";
     } else {
         // 使用者未登入
         console.log("使用者尚未登入");
+        document.getElementById("login_statue").innerHTML="尚未登入";
     }
 });
