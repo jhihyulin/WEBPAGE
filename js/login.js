855781aa572e6f39ea5459da473d93327d562d13
jhih_yu <74854953+jhihyulin@users.noreply.github.com>
2021-08-03 18:02:00 +0800

    login test

--- js/login.js
+++ js/login.js
@@ -216,6 +216,7 @@ function change_password() {
         });
 }
 
+/*
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // 使用者已登入，可以取得資料
@@ -236,6 +237,7 @@ firebase.auth().onAuthStateChanged(function (user) {
         document.getElementById("login_statue").innerHTML = "尚未登入";
     }
 });
+*/
 
 firebase.auth().onAuthStateChanged(function (user) {
     try {
@@ -293,3 +295,37 @@ function reAuth(checkPassword) {
         });
     })
 }
+
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var uid = user.uid;
+        var phoneNumber = user.phoneNumber;
+        var providerData = user.providerData;
+        user.getIdToken().then(function (accessToken) {
+            document.getElementById('sign-in-status').textContent = 'Signed in';
+            document.getElementById('sign-in').textContent = 'Sign out';
+            document.getElementById('account-details').textContent = JSON.stringify({
+                displayName: displayName,
+                email: email,
+                emailVerified: emailVerified,
+                phoneNumber: phoneNumber,
+                photoURL: photoURL,
+                uid: uid,
+                accessToken: accessToken,
+                providerData: providerData
+            }, null, '  ');
+        });
+    } else {
+        // User is signed out.
+        document.getElementById('sign-in-status').textContent = 'Signed out';
+        document.getElementById('sign-in').textContent = 'Sign in';
+        document.getElementById('account-details').textContent = 'null';
+    }
+}, function (error) {
+    console.log(error);
+});
