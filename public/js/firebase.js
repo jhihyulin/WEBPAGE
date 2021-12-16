caa98dc12d15092cf53d9462dbf9a911db3dbddd
jhih_yu <74854953+jhihyulin@users.noreply.github.com>
2021-12-16 23:17:26 +0800

    UPDATE

---
+++ public/js/firebase.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCD9r1BoMkQ7_lzschLnh-rYaMsA7LpEQs",
+  authDomain: "webpage-349c7.firebaseapp.com",
+  databaseURL: "https://webpage-349c7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "webpage-349c7",
+  storageBucket: "webpage-349c7.appspot.com",
+  messagingSenderId: "897798864282",
+  appId: "1:897798864282:web:4ee8989435b61290f10e23",
+  measurementId: "G-JDZTQWRMLT"
+};
+
+try {
+    firebase.initializeApp(firebaseConfig);
+    var perf = firebase.performance();
+} catch (e) {
+    console.log(e);
+}
