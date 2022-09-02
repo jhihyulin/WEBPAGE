304dfb464537494bfeb522199868cbd05b29f1a7
thomas.JY.Lin@gmail.com <thomas.JY.Lin@gmail.com>
2022-09-02 09:31:47 +0800

    add console info js

---
+++ public/js/console_info.js
@@ -0,0 +1,3 @@
+console.info("CREDIT:\nDimension by HTML5 UP\nhtml5up.net | @ajlkn\nFree for personal and commercial use under the CCA 3.0 license (html5up.net/license)")
+console.info("LICENSE:\n@jhihyulin\nAttribution 3.0 Taiwan (CC BY 3.0 TW) (https://creativecommons.org/licenses/by/3.0/tw/legalcode)")
+console.info("%c⚠️警告⚠️\n%c如果你不知道你在做什麼，請不要在此複製或輸入任何內容。\n這是給開發人員的瀏覽器功能，如果有人告訴您在此處複製或貼上某些內容以啟用某些功能或「破解」某人的帳戶，你將給予他們你帳戶的使用權限。", "color: #ff0000; background: #ffff00", "color: #000000; background: #ffff00")
